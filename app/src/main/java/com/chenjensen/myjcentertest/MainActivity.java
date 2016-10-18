d0ac0c84d1bcbac316c805364379bfff9086a50d
Jensenczx <jensenczx@gmail.com>
2016-10-18 14:28:48 +0800

    first commit

---
+++ app/src/main/java/com/chenjensen/myjcentertest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.chenjensen.myjcentertest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.chenjensen.mylibrary.TestClass;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ mylibrary/src/main/java/com/chenjensen/mylibrary/TestClass.java
@@ -0,0 +1,11 @@
+package com.chenjensen.mylibrary;
+
+/**
+ * Created by chenjensen on 16/10/18.
+ */
+public class TestClass {
+
+    public String getText() {
+        return "TestClass";
+    }
+}
